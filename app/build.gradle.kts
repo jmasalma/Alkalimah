8ae04417542fba8bc37697a0caf803a970e233f3
JM <jehad.masalma@sap.com>
2025-12-20 18:56:09 -0500

    minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ android {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -57,6 +62,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
